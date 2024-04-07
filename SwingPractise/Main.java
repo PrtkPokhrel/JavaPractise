f36131835fefddc5f31b8b0808b2915ec0bc34c0
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-04-07 09:53:00 +0545

    update

--- SwingPractise/Main.java
+++ SwingPractise/Main.java
@@ -1,8 +1,22 @@
 import javax.swing.JFrame;
-public class Main{
+import javax.swing.ImageIcon;
+import java.awt.Color;
+
+public class Main {
     public static void main(String[] args) {
-JFrame frame=new JFrame();
- frame.setVisible(true);
- 
+        JFrame frame = new JFrame(); // creates a frame
+        frame.setVisible(true); // make frame visible
+        frame.setSize(400, 400); // sets x dimension and y dimension of frame
+        frame.setTitle("This is the title"); // sets title of the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closes the application when exit is clicked
+        frame.setResizable(false); // prevent frame from being resized
+
+        ImageIcon img = new ImageIcon("porce.jpg");
+        frame.setIconImage(img.getImage()); // Sets the Icon image
+        // frame.getContentPane().setBackground(Color.green); //Sets the backgorund
+        // color of the frame
+        frame.getContentPane().setBackground(new Color(81, 1, 120)); // different way to set the background color
+          
+
     }
 }
