2690a5c2945761a783e2df8203a90b21a7d755e3
PrtkPokhrel <pokhrelpratik52@gmail.com>
2024-04-10 20:21:55 +0545

    more

---
+++ Practise1/Car.java
@@ -0,0 +1,21 @@
+package Practise1;
+
+public class Car{
+ String carName="Porche";
+ int backName=911;
+ Boolean superCar=true;
+ 
+ public String start(){
+    return "The car is Starting: ";
+ }
+ public String brake(){
+    return "The brake is pressed: ";
+ }
+}
+
+// Class:
+// Blueprint
+// Object
+// Instance
+// Method
+// Attribute
---
+++ Practise1/Main.java
@@ -0,0 +1,10 @@
+package Practise1;
+public class Main{
+    public static void main(String[] args) {
+        Car mycar =new Car();
+       System.out.println(mycar.carName);
+
+
+    }
+
+}
